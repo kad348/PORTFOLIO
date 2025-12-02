3b5509adf70a629e5fe27481d46dedcc9587d47b
kad348 <kadithya348@gmail.com>
2025-12-02 19:56:23 +0530

    Add images

--- data/designs.ts
+++ data/designs.ts
@@ -7,7 +7,7 @@ export const designProjects: Project[] = [
     id: 5,
     title: 'Sweet Freeze Campaign',
     category: 'graphic',
-    image: 'https://cdn.discordapp.com/attachments/1076791764167434253/1444442793467707576/sweet-freeze.jpg?ex=692cb9ae&is=692b682e&hm=62bf9d257d6a5751f00bd89088f7f9f683e2c2425b2df1ea2f5f8caaae10bcdd&',
+    image: '/images/sweet-freeze.jpg',
     description: 'Vibrant event poster design for ICBT Campus ice cream stall featuring custom typography.',
     link: '/images.rar'
   },
@@ -15,7 +15,7 @@ export const designProjects: Project[] = [
     id: 6,
     title: 'SparePoint Product Ad',
     category: 'graphic',
-    image: 'https://cdn.discordapp.com/attachments/1076791764167434253/1440470846865932371/NISSAN_X_2.png?ex=692d6fc4&is=692c1e44&hm=162eec976c21f514c4653ad727135287031ff04cba3172ca622b98ee020b38c6&',
+    image: '/images/NISSAN_X_2.png',
     description: 'Professional product advertisement for Nissan X-Trail automotive parts.',
     link: '/images.rar'
   },
@@ -23,8 +23,16 @@ export const designProjects: Project[] = [
     id: 7,
     title: 'Hotel Seetha Flyer',
     category: 'graphic',
-    image: 'https://cdn.discordapp.com/attachments/1076791764167434253/1444442047263014932/hotel-flyer.jpg?ex=692cb8fc&is=692b677c&hm=5c49d50ea555851421438dace16674a8b48ac26ebf01848658669259c535337d&',
+    image: '/images/hotel-flyer.jpg',
     description: 'Elegant flyer design showcasing hotel features, services, and seasonal offers.',
+    link: '/images.rar'
+  },
+  {
+    id: 7,
+    title: 'Lewis Hamilton Poster',
+    category: 'graphic',
+    image: '/images/f1.png',
+    description: 'F1 Poster for Reasons Podcast.',
     link: '/images.rar'
   },
 ];

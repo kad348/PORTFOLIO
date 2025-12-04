83ca6615842d662396275af7666243f91f570df1
kad348 <kadithya348@gmail.com>
2025-12-05 02:24:59 +0530

    Update designs.ts

--- data/designs.ts
+++ data/designs.ts
@@ -7,7 +7,7 @@ export const designProjects: Project[] = [
     id: 5,
     title: 'Sweet Freeze Campaign',
     category: 'graphic',
-    image: 'https://github.com/kad348/Work/blob/main/sweet-freeze.jpg',
+    image: 'https://cdn.discordapp.com/attachments/1064504984076566540/1445815344500310036/sweet-freeze.jpg?ex=69330978&is=6931b7f8&hm=423b487627042d53f0635badffe7e440cd6e27f2c3b18ed3d80fab7223a59865&',
     description: 'Vibrant event poster design for ICBT Campus ice cream stall featuring custom typography.',
   },
   {
